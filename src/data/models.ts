0bb461a3e7d5e5cb1bbd7eeb3663887693f34d4d
LindForce <joakim.lindfors2@gmail.com>
2025-04-04 14:29:44 +0200

    added more models

--- src/data/models.ts
+++ src/data/models.ts
@@ -1,24 +1,31 @@
 // src/data/models.ts
 
-import RedTree from '../assets/3d-models/red_tree.glb'; // Example model, replace with your actual model
-import BlueGem from '../assets/3d-models/blue_gem.glb'; // Example model, replace with your actual model
-import FruitBasket from '../assets/3d-models/fruit_basket.glb'; // Example model, replace with your actual model
-import TreasureChest from '../assets/3d-models/treasure_chest.glb'; // Example model, replace with your actual model
+import RedTreeL from '../assets/3d-models/lumaai/red_tree_l.glb'; // Example model, replace with your actual model
+import TreasureChestL from '../assets/3d-models/lumaai/treasure_chest_l.glb'; // Example model, replace with your actual model
+
+import RedTreeH from '../assets/3d-models/hunyuan3d/red_tree.glb'; // Example model, replace with your actual model
+import BlueGemH from '../assets/3d-models/hunyuan3d/blue_gem.glb'; // Example model, replace with your actual model
+import FruitBasketH from '../assets/3d-models/hunyuan3d/fruit_basket.glb'; // Example model, replace with your actual model
+import TreasureChestH from '../assets/3d-models/hunyuan3d/treasure_chest.glb'; // Example model, replace with your actual model
 
 interface ModelData {
     [key: number]: string;
 }
 
 export const modelUrls: ModelData = {
-    1: RedTree,
-    2: BlueGem,
-    3: FruitBasket,
-    4: TreasureChest,
+    1: RedTreeL,
+    2: TreasureChestL,
+    3: RedTreeH,
+    4: TreasureChestH,
+    5: FruitBasketH,
+    6: BlueGemH,
 };
 
 export const modelDescriptions: Record<number, string> = {
     1: 'A red fantasy tree with twisted roots',
-    2: 'A blue sapphire spiral gem',
-    3: 'A fruit basket filled with various fruits',
+    2: 'A treasure chest filled with gold coins',
+    3: 'A red fantasy tree with twisted roots',
     4: 'A treasure chest filled with gold coins',
+    5: 'A fruit basket filled with various fruits',
+    6: 'A blue sapphire spiral gem',
 };
