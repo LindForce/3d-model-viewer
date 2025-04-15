a0c34a32b2303ac6bcb6483a4ee78c8dd440da64
Joakim Lindfors <joakim.lindfors2@gmail.com>
2025-04-15 11:32:03 +0200

    updated instructions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,8 +11,8 @@ export default defineComponent({
     <div class="mt-6 p-4 bg-blue-50 border-l-4 border-blue-500 rounded">
         <h3 class="font-medium text-blue-800">Instructions</h3>
         <p class="text-blue-700">
-            Please examine this model carefully, then return to the Microsoft
-            Forms survey to fill in the evaluation.
+            When you have made it to Part 1 in the survey, please navigate to
+            Part 1 in the menu above.
         </p>
     </div>
 </template>
